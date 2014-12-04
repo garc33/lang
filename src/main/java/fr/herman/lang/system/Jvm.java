addc5341af8541b8dd53be48d52aeb017f0a4455
Marc Herman <garc33@gmail.com>
2014-12-04 18:04:56 +0100

    file properties

--- src/main/java/fr/herman/lang/system/Jvm.java
+++ src/main/java/fr/herman/lang/system/Jvm.java
@@ -5,6 +5,10 @@ public enum Jvm {
 
     public static final String TMP_DIR = System.getProperty("java.io.tmpdir");
 
+    public static final String FILE_ENCODING = System.getProperty("file.encoding");
+
+    public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+
     private static final boolean IS_64_BIT = resolve64Bit();
 
     private static final JavaVersion JAVA_VERSION = JavaVersion.resolve(System.getProperty("java.version"));
