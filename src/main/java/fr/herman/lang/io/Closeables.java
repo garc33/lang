11557f75342c8517a8585375ff843286481a7c70
Marc Herman <garc33@gmail.com>
2014-12-04 16:59:02 +0100

    improve closeables

--- src/main/java/fr/herman/lang/io/Closeables.java
+++ src/main/java/fr/herman/lang/io/Closeables.java
@@ -1,15 +1,27 @@
 package fr.herman.lang.io;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 public class Closeables {
     private Closeables() {
 
     }
 
-    public static void closeQuietly(Closeable closeable) {
+    public static void close(Closeable closeable) {
+        if (closeable == null) {
+            return;
+        }
         try {
             closeable.close();
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static void closeQuietly(Closeable closeable) {
+        try {
+            close(closeable);
         } catch (Throwable e) {
 
         }
